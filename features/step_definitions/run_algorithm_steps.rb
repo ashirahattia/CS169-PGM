d265faa6f8033ea943fb2b2470b0caaf66be127a
Angira Shirahatti <ashirahattia@berkeley.edu>
2016-10-15 04:25:23 +0000

    removing some comments

--- features/step_definitions/run_algorithm_steps.rb
+++ features/step_definitions/run_algorithm_steps.rb
@@ -1,35 +1,35 @@
 Given(/^I am logged in as a TA$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  pending 
 end
 
 Given(/^there is no existing project\-group match$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  pending 
 end
 
 Then(/^I should see Run Algorithm$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  pending
 end
 
 Given(/^I have run the algorithm$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  pending 
 end
 
 Then(/^I should see a project\-group match table$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  pending 
 end
 
 Given(/^I can see a table$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  pending 
 end
 
 Then(/^I should see Re\-Run Algorithm$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  pending 
 end
 
 Then(/^I press Re\-Run Algorithm$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  pending 
 end
 
 Then(/^I should see a different table$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  pending 
 end
