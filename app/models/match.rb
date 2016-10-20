664b725bff3f63a3df5b39fc7af4ba0517975c54
Trevor Nesbitt <tnesbitt@berkeley.edu>
2016-10-20 04:16:11 +0000

    created not-so-great algorithm

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,2 +1,12 @@
 class Match < ActiveRecord::Base
+    def self.algorithm
+        Match.delete_all
+        all_projects = Project.all
+        all_groups = Group.all
+        all_groups.count.times do |index|
+            group_name = all_groups[index].group_name
+            project_name = all_projects[index].project_name
+            Match.create(:group_name => group_name, :project_name => project_name)
+        end
+    end
 end
