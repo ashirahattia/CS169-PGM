1c393de30214b527f8a830a82025a00a8c808e39
Kadhir <kadhirvelm@gmail.com>
2016-11-03 00:26:12 +0000

    Project UI Fixes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,12 +1,14 @@
 class ProjectsController < ApplicationController
     def index
+        @projects = Project.all
     end
     
     def new
         if params.key?(:project_name)
             name = params[:project_name]
             project = Project.create(project_name: name)
-            flash[:notice] = "Project " + name + " successfully added"
+            # flash[:notice] = "Project " + name + " successfully added"
+            redirect_to projects_path
         end
     end
     
