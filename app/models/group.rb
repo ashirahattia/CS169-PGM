bf435608422f83346bf67dd69f881add7c6c80f7
Ying Luo <ilyluo@berkeley.edu>
2016-11-15 22:19:30 +0000

    Finished implementing Hungarian algorithm, though will need to address some edge cases later on

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,15 +1,2 @@
 class Group < ActiveRecord::Base
-    attr_accessor :first_choice
-    attr_accessor :second_choice
-    attr_accessor :third_choice
-    attr_accessor :fourth_choice
-    attr_accessor :fifth_choice
-    attr_accessor :sixth_choice
-    attr_accessor :seventh_choice
-    attr_accessor :group_name
-    
-    def initialize
-        # Set choices based on Google Form
-    end
-    
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -5,12 +5,11 @@ class Match < ActiveRecord::Base
     $all_projects = Project.all
     $all_groups = Group.all
     
-    
-    def generate_preference_list(group)
-        group_preferences = [group.first_choice, group.second_choice,
-                            group.third_choice, group.fourth_choice,
-                            group.fifth_choice, group.sixth_choice,
-                            group.seventh_choice]
+    def self.generate_preference_list(group)
+        group_preferences = [group[:first_choice], group[:second_choice],
+                            group[:third_choice], group[:fourth_choice],
+                            group[:fifth_choice], group[:sixth_choice],
+                            group[:seventh_choice]]
         return group_preferences
     end
     
@@ -19,46 +18,61 @@ class Match < ActiveRecord::Base
     # column corresponds to a project. Each cell represents a group's ranking
     # for that project, where if a project is not ranked by the group, then
     # it is given a cost of 100.
-    def generate_cost_matrix
+    def self.generate_cost_matrix
         cost_matrix = Array.new
-        all_groups.each do |group|
-            project_list = generate_preference_list(group)
+        $all_groups.each do |group|
+            project_list = self.generate_preference_list(group)
             row = Array.new
-            all_projects.count.times do |index|
-                curr_project = all_projects[index]
-                if project_list.include?(curr_project)
-                    rank = project_list.index(curr_project) + 1
+            $all_projects.count.times do |index|
+                curr_project = $all_projects[index]
+                if project_list.include?(curr_project.id)
+                    rank = project_list.index(curr_project.id) + 1
                     row << rank
                 else 
                     row << 100
                 end 
             end
             cost_matrix << row
+        end
         return cost_matrix
+    end
+   
     
-    
-    def self.algorithm
-        cost_matrix = generate_cost_matrix
-        m = Munkres.new(cost_matrix)
-        puts m.find_pairings
-        # m.find_pairings.each do |group, project|
-        #     create_match(group, project)
-        # end
+    # Fill in cost matrix with dummy groups to create an nxn cost matrix
+    # since there will probably be more projects than groups
+    def self.fill_dummy_groups(cost_matrix)
+        num_projects = cost_matrix[0].length
+        while num_projects > cost_matrix.length
+            dummy_row = self.make_dummy_arr(num_projects)
+            cost_matrix << dummy_row
+        end 
+        return cost_matrix
     end
     
     
-    def create_match(group_name, project_name)
-        Match.create(:group_name => group_name, :project_name => project_name)
-    end 
+    # Helper method for fill_in_dummy_groups
+    def self.make_dummy_arr(length)
+        return Array.new(length, 500)
+    end
+
     
-    # def self.algorithm
-    #     Match.delete_all
-    #     all_projects = Project.all.shuffle
-    #     all_groups = Group.all
-    #     all_groups.count.times do |index|
-    #         group_name = all_groups[index].group_name
-    #         project_name = all_projects[index].project_name
-    #         Match.create(:group_name => group_name, :project_name => project_name)
-    #     end
-    # end
+    # Converts results matrix to matches and filters out dummy groups created
+    def self.convert_matrix_to_matches(results_matrix)
+        results_matrix.each do |group_id, project_id|
+            if group_id < $all_groups.length
+                group_name = $all_groups[group_id].group_name
+                project_name = $all_projects[project_id].project_name
+                Match.create(:group_name => group_name, :project_name => project_name)
+            end
+        end
+    end
+    
+    
+    def self.algorithm
+        Match.delete_all
+        cost_matrix = self.fill_dummy_groups(self.generate_cost_matrix)
+        m = Munkres.new(cost_matrix)
+        self.convert_matrix_to_matches(m.find_pairings)
+    end
+
 end
