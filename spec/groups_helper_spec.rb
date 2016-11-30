837fa66cc7b899f1d5ac08842074fc73bb71c135
Trevor Nesbitt <tnesbitt@berkeley.edu>
2016-11-30 23:45:52 +0000

    hopefully 100% test coverage for groups helper

--- spec/groups_helper_spec.rb
+++ spec/groups_helper_spec.rb
@@ -9,4 +9,22 @@ describe GroupsHelper, :type => :helper do
         helper.choice_str(:second_choice).should eql("not chosen")
         helper.choice_str(:first_choice).should eql("Project1")
     end
+    
+        it 'selects if needed' do
+        helper.select_if_needed(1, 1).should eql(", selected=selected")
+        helper.select_if_needed(1, 2).should eql("")
+    end
+    
+    it 'gets the current force matched project' do
+        group_without_force_match = Group.new()
+        group_with_force_match = Group.new()
+        project_match = Project.new(:project_name => "i like poop")
+        allow(group_with_force_match).to receive(:force_matched_project) {project_match}
+        
+        @group = group_without_force_match
+        helper.current_force_matched_project.should eql("None")
+        
+        @group = group_with_force_match
+        helper.current_force_matched_project.should eql("i like poop")
+    end
 end
