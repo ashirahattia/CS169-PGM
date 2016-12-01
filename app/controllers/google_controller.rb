c2a1676bfe973e76819236bd83d89fee6a2e112d
Trevor Nesbitt <tnesbitt@berkeley.edu>
2016-12-01 02:23:33 +0000

    fixed logic

--- app/controllers/google_controller.rb
+++ app/controllers/google_controller.rb
@@ -58,7 +58,7 @@ class GoogleController < ApplicationController
 
   def create_projects(response)
     response.values.each do |row|
-      row[@@SETTINGS.project_id_col.to_i].nil? ? nil : Project.create(:id => row[@@SETTINGS.project_id_col.to_i],
+      row[@@SETTINGS.project_id_col.to_i].nil? ? (return) : Project.create(:id => row[@@SETTINGS.project_id_col.to_i],
                      :project_name => row[@@SETTINGS.project_name_col.to_i])
     end
   end
