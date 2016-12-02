08c23c396f0f54e4f143d12e2a8d575d4346ca42
Trevor Nesbitt <tnesbitt@berkeley.edu>
2016-12-02 21:20:36 +0000

    changed session variables to class variables

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -3,21 +3,25 @@ class MatchesController < ApplicationController
     
     before_filter :check_logged_in, :except => :index
     
+    @@x = "1.0"
+    @@exponential = ""
+    @@power = "checked=checked"
+
     def new
         if params[:loss_function] == "exponential"
             loss_function = :exponential
-            session[:exponential] = "checked=checked"
-            session[:power] = ""
+            @@exponential = "checked=checked"
+            @@power = ""
         else
             loss_function = :power
-            session[:power] = "checked=checked"
-            session[:exponential] = ""
+            @@power = "checked=checked"
+            @@exponential = ""
         end
         
         x = params[:x].to_f
         x = [x, 1.0e-14].max # x shouldn't be negative
         x = [x, 10].min # if x is too high, shit crashes. and results converge anyway so...
-        session[:x] = x.to_s
+        @@x = x.to_s
         
         Match.algorithm(loss_function, x)
         redirect_to '/matches'
